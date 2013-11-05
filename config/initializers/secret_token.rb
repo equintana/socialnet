71bacb7a7854b80581588111996d8d0ba6af8321
Ricardo <rickardoberdejo@hotmail.com>
2013-11-05 17:20:30 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialnet::Application.config.secret_token = '413e34dc0e757a110df27842119709ac37e02aa636c016e83edbde45d42139abae41ccd31eaec12cad6e3540afd5cece988f72e1dd1e6287604fd90c621b9038'
