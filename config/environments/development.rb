e95b622bf861fee6c7057112bd22639645202e28
Ricardo <rickardoberdejo@hotmail.com>
2013-11-22 09:15:52 -0500

    remove sending mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,5 +38,7 @@ Socialnet::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   # show errors when sending mails
   config.action_mailer.raise_delivery_errors = true
+  
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
